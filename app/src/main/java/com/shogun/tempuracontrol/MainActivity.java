babea0f49b735b683e6545ec5daafdb383a75920
ShogunRoss <nguyenconglong95@gmail.com>
2017-08-30 14:08:44 +0700

    Third Commit.

--- app/src/main/java/com/shogun/tempuracontrol/MainActivity.java
+++ app/src/main/java/com/shogun/tempuracontrol/MainActivity.java
@@ -1,4 +1,5 @@
 package com.shogun.tempuracontrol;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothDevice;
@@ -6,7 +7,6 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -36,11 +36,19 @@ import java.util.LinkedList;
 import java.util.Set;
 
 
-public class MainActivity extends AppCompatActivity {
-	
+/**
+ * Blutooth Arduino Echo.
+ *
+ * 문자열의 끝은 '\0' 을 붙여서 구분한다.
+ *
+ * www.dev.re.kr
+ * @author ice3x2@gmail.com / Beom
+ */
+public class MainActivity extends Activity {
+
 	private LinkedList<BluetoothDevice> mBluetoothDevices = new LinkedList<BluetoothDevice>();
 	private ArrayAdapter<String> mDeviceArrayAdapter;
-	
+
 	private EditText mEditTextInput;
 	private TextView mTextView;
 	private Button mButtonSend;
@@ -48,56 +56,59 @@ public class MainActivity extends AppCompatActivity {
 	private AlertDialog mDeviceListDialog;
 	private Menu mMenu;
 	private BluetoothSerialClient mClient;
-	
-	
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		 super.onCreate(savedInstanceState);
-		 setContentView(R.layout.activity_main);
-		 mClient = BluetoothSerialClient.getInstance();
-		 
-		 if(mClient == null) {
-			 Toast.makeText(getApplicationContext(), "Cannot use the Bluetooth device.", Toast.LENGTH_SHORT).show();
-			 finish();
-		 }
-		 overflowMenuInActionBar();
-		 initProgressDialog();
-		 initDeviceListDialog();
-		 initWidget();
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mClient = BluetoothSerialClient.getInstance();
+
+		if(mClient == null) {
+			Toast.makeText(getApplicationContext(), "Cannot use the Bluetooth device.", Toast.LENGTH_SHORT).show();
+			finish();
+		}
+		overflowMenuInActionBar();
+		initProgressDialog();
+		initDeviceListDialog();
+		initWidget();
+
 	}
-	
+
 	private void overflowMenuInActionBar(){
-		 try {
-		        ViewConfiguration config = ViewConfiguration.get(this);
-		        Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
-		        if(menuKeyField != null) {
-		            menuKeyField.setAccessible(true);
-		            menuKeyField.setBoolean(config, false);
-		        }
-		    } catch (Exception ex) {
-		    }
+		try {
+			ViewConfiguration config = ViewConfiguration.get(this);
+			Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+			if(menuKeyField != null) {
+				menuKeyField.setAccessible(true);
+				menuKeyField.setBoolean(config, false);
+			}
+		} catch (Exception ex) {
+			// 무시한다. 3.x 이 예외가 발생한다.
+			// 또, 타블릿 전용으로 만들어진 3.x 버전의 디바이스는 보통 하드웨어 버튼이 존재하지 않는다.
+		}
 	}
-	
-	
+
+
 	@Override
 	protected void onPause() {
 		mClient.cancelScan(getApplicationContext());
 		super.onPause();
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-	enableBluetooth();
+		enableBluetooth();
 
-		
+
 	}
-	
+
 	private void initProgressDialog() {
-		 mLoadingDialog = new ProgressDialog(this);
-		 mLoadingDialog.setCancelable(false);
+		mLoadingDialog = new ProgressDialog(this);
+		mLoadingDialog.setCancelable(false);
 	}
-	
+
 	private void initWidget() {
 		mTextView = (TextView) findViewById(R.id.textViewTerminal);
 		mTextView.setMovementMethod(new ScrollingMovementMethod());
@@ -106,12 +117,12 @@ public class MainActivity extends AppCompatActivity {
 		mButtonSend.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				sendStringData(mEditTextInput.getText().toString() + "\n");
+				sendStringData(mEditTextInput.getText().toString());
 				mEditTextInput.setText("");
 			}
 		});
 	}
-	
+
 	private void initDeviceListDialog() {
 		mDeviceArrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.item_device);
 		ListView listView = new ListView(getApplicationContext());
@@ -119,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
 		listView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				String item =  (String) parent.getItemAtPosition(position); 
+				String item =  (String) parent.getItemAtPosition(position);
 				for(BluetoothDevice device : mBluetoothDevices) {
 					if(item.contains(device.getAddress())) {
 						connect(device);
@@ -132,27 +143,30 @@ public class MainActivity extends AppCompatActivity {
 		builder.setTitle("Select bluetooth device");
 		builder.setView(listView);
 		builder.setPositiveButton("Scan",
-		 new DialogInterface.OnClickListener() {
-		  public void onClick(DialogInterface dialog, int id) {
-			  scanDevices();
-		  }
-		 });
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						scanDevices();
+					}
+				});
 		mDeviceListDialog = builder.create();
 		mDeviceListDialog.setCanceledOnTouchOutside(false);
 	}
-	
+
 	private void addDeviceToArrayAdapter(BluetoothDevice device) {
-		if(mBluetoothDevices.contains(device)) { 
+		if(mBluetoothDevices.contains(device)) {
 			mBluetoothDevices.remove(device);
 			mDeviceArrayAdapter.remove(device.getName() + "\n" + device.getAddress());
 		}
-			mBluetoothDevices.add(device);
-			mDeviceArrayAdapter.add(device.getName() + "\n" + device.getAddress() );
-			mDeviceArrayAdapter.notifyDataSetChanged();
-		
+		mBluetoothDevices.add(device);
+		mDeviceArrayAdapter.add(device.getName() + "\n" + device.getAddress() );
+		mDeviceArrayAdapter.notifyDataSetChanged();
+
 	}
 
-	
+
+
+
+
 	private void enableBluetooth() {
 		BluetoothSerialClient btSet =  mClient;
 		btSet.enableBluetooth(this, new OnBluetoothEnabledListener() {
@@ -166,24 +180,24 @@ public class MainActivity extends AppCompatActivity {
 			}
 		});
 	}
-	
+
 	private void addText(String text) {
-	    mTextView.append(text);
-	    final int scrollAmount = mTextView.getLayout().getLineTop(mTextView.getLineCount()) - mTextView.getHeight();
-	    if (scrollAmount > 0)
-	    	mTextView.scrollTo(0, scrollAmount);
-	    else
-	    	mTextView.scrollTo(0, 0);
+		mTextView.append(text);
+		final int scrollAmount = mTextView.getLayout().getLineTop(mTextView.getLineCount()) - mTextView.getHeight();
+		if (scrollAmount > 0)
+			mTextView.scrollTo(0, scrollAmount);
+		else
+			mTextView.scrollTo(0, 0);
 	}
-	
-	
+
+
 	private void getPairedDevices() {
 		Set<BluetoothDevice> devices =  mClient.getPairedDevices();
 		for(BluetoothDevice device: devices) {
 			addDeviceToArrayAdapter(device);
 		}
 	}
-	
+
 	private void scanDevices() {
 		BluetoothSerialClient btSet = mClient;
 		btSet.scanDevices(getApplicationContext(), new OnScanListener() {
@@ -204,14 +218,14 @@ public class MainActivity extends AppCompatActivity {
 					}
 				});
 			}
-			
+
 			@Override
 			public void onFoundDevice(BluetoothDevice bluetoothDevice) {
 				addDeviceToArrayAdapter(bluetoothDevice);
 				message += "\n" + bluetoothDevice.getName() + "\n" + bluetoothDevice.getAddress();
 				mLoadingDialog.setMessage(message);
 			}
-			
+
 			@Override
 			public void onFinish() {
 				Log.d("Test", "Scan finish.");
@@ -223,8 +237,8 @@ public class MainActivity extends AppCompatActivity {
 			}
 		});
 	}
-	
-	
+
+
 	private void connect(BluetoothDevice device) {
 		mLoadingDialog.setMessage("Connecting....");
 		mLoadingDialog.setCancelable(false);
@@ -232,47 +246,47 @@ public class MainActivity extends AppCompatActivity {
 		BluetoothSerialClient btSet =  mClient;
 		btSet.connect(getApplicationContext(), device, mBTHandler);
 	}
-	
+
 	private BluetoothStreamingHandler mBTHandler = new BluetoothStreamingHandler() {
 		ByteBuffer mmByteBuffer = ByteBuffer.allocate(1024);
-		
+
 		@Override
 		public void onError(Exception e) {
 			mLoadingDialog.cancel();
-			addText("Message : Connection error - " +  e.toString() + "\n");
+			addText("Messgae : Connection error - " +  e.toString() + "\n");
 			mMenu.getItem(0).setTitle(R.string.action_connect);
 		}
-		
+
 		@Override
 		public void onDisconnected() {
 			mMenu.getItem(0).setTitle(R.string.action_connect);
 			mLoadingDialog.cancel();
-			addText("Message : Disconnected.\n");
+			addText("Messgae : Disconnected.\n");
 		}
 		@Override
 		public void onData(byte[] buffer, int length) {
 			if(length == 0) return;
 			if(mmByteBuffer.position() + length >= mmByteBuffer.capacity()) {
-				ByteBuffer newBuffer = ByteBuffer.allocate(mmByteBuffer.capacity() * 2); 
+				ByteBuffer newBuffer = ByteBuffer.allocate(mmByteBuffer.capacity() * 2);
 				newBuffer.put(mmByteBuffer.array(), 0,  mmByteBuffer.position());
 				mmByteBuffer = newBuffer;
-			} 
+			}
 			mmByteBuffer.put(buffer, 0, length);
 			if(buffer[length - 1] == '\0') {
 				addText(mClient.getConnectedDevice().getName() + " : " +
-						new String(mmByteBuffer.array(), 0, mmByteBuffer.position()) + '\n'); 
+						new String(mmByteBuffer.array(), 0, mmByteBuffer.position()) + '\n');
 				mmByteBuffer.clear();
 			}
 		}
-		
+
 		@Override
 		public void onConnected() {
-			addText("Message : Connected. " + mClient.getConnectedDevice().getName() + "\n");
+			addText("Messgae : Connected. " + mClient.getConnectedDevice().getName() + "\n");
 			mLoadingDialog.cancel();
 			mMenu.getItem(0).setTitle(R.string.action_disconnect);
 		}
 	};
-	
+
 	public void sendStringData(String data) {
 		data += '\0';
 		byte[] buffer = data.getBytes();
@@ -280,7 +294,7 @@ public class MainActivity extends AppCompatActivity {
 			addText("Me : " + data + '\n');
 		}
 	}
-	
+
 	protected void onDestroy() {
 		super.onDestroy();
 		mClient.claer();
@@ -316,17 +330,17 @@ public class MainActivity extends AppCompatActivity {
 		codeView.setMovementMethod(new ScrollingMovementMethod());
 		codeView.setBackgroundColor(Color.parseColor("#202020"));
 		new AlertDialog.Builder(this, android.R.style.Theme_Holo_Light_DialogWhenLarge)
-		.setView(codeView)
-		.setPositiveButton("OK", new AlertDialog.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.cancel();
-			}
-		}).show();
+				.setView(codeView)
+				.setPositiveButton("OK", new AlertDialog.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+					}
+				}).show();
 	}
-	
+
 	private String readCode() {
-		 try {
+		try {
 			InputStream is = getAssets().open("About.txt");
 			int length = is.available();
 			byte[] buffer = new byte[length];
@@ -338,32 +352,8 @@ public class MainActivity extends AppCompatActivity {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		 return "";
+		return "";
 	}
-	
+
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
